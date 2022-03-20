38036dbb8f7eb80856fc89ddd1e53603bd2e6a8a
vlsalina <vlsalina@ucsd.edu>
2022-03-20 15:17:53 -0700

    Styling for top pane done.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <main className="main c">Main</main>;
+  return (
+    <main className="main c">
+      <div>
+        <h3 className="main__header">career objectives</h3>
+      </div>
+      <div>
+        <h3 className="main__header">experience</h3>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
--- src/components/SidePane/SidePane.js
+++ src/components/SidePane/SidePane.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./SidePane.css";
+import { BsHexagonFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 const SidePane = () => {
   return <section className="side-pane b">SidePane</section>;
